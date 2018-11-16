9ac5284c535f4effb9deef91a62d6f3ad1a2a780
Jonathan Wong <34286815+jonwong-tealium@users.noreply.github.com>
2018-11-16 09:01:05 -0800

    Release 5.4.6. (#88)

---
+++ DevicesOnly/TealiumIOS.framework/Headers/TEALDispatch.h
@@ -0,0 +1,143 @@
+//
+//  TEALDispatch.h
+//  TealiumUtilities
+//
+//  Created by George Webster on 2/13/15.
+//  Copyright (c) 2015 Tealium Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+extern NSString * _Nonnull const TEALDispatchKey_Dispatch_Type ;
+extern NSString * _Nonnull const TEALDispatchKey_Dispatch_Service ;
+extern NSString * _Nonnull const TEALDispatchKey_DataSources_Payload;
+extern NSString * _Nonnull const TEALDispatchKey_Time_Stamp;
+
+
+/**
+ *  Types of available dispatches
+ */
+typedef NS_ENUM(NSUInteger, TEALDispatchType) {
+    /**
+     *  Dispatch type used to track monitiored behavioral actions.
+     */
+    TEALDispatchTypeActivity,
+    /**
+     *   Dispatch type that represents data about the user that was infered.
+     */
+    TEALDispatchTypeDerived,
+    /**
+     *  Dispatch for view / screen appearances only.
+     */
+    TEALDispatchTypeView,
+    /**
+     * Dispatch type when an event represents the interaction between a user and an external resource
+     */
+    TEALDispatchTypeInteraction,
+    /**
+     * Dispatch type that represents events that dictate that a desired goal has been reached
+     */
+    TEALDispatchTypeConversion
+};
+
+/**
+ *  Dispatch Status Types
+ */
+typedef NS_ENUM(NSUInteger, TEALDispatchStatus) {
+    /**
+     *  Status not known.
+     */
+    TEALDispatchStatusUnknown = 0,
+    /**
+     *  Delivered dispatch.
+     */
+    TEALDispatchStatusSent,
+    /**
+     *  Was saved.
+     */
+    TEALDispatchStatusQueued,
+    /**
+     *  Destruction requested.
+     */
+    TEALDispatchStatusDestroyed,
+    /**
+     *  Other failure.
+     */
+    TEALDispatchStatusFailed
+};
+
+
+/**
+ *  Constants for the two most common dispatch types
+ *
+ */
+
+@interface TEALDispatch : NSObject <NSCoding>
+
+typedef void (^TEALDispatchBlock)(TEALDispatchStatus status, TEALDispatch * _Nonnull dispatch, NSError * _Nullable error);
+
+/**
+ *  Type of dispatch, either 'activity', 'view' , 'interaction', 'derived', or 'conversion'
+ */
+@property (nonatomic) TEALDispatchType dispatchType;
+
+/**
+ *  Optional block assigned to dispatch that can be triggered upon dispatch or queuing of dispatch
+ *
+ */
+@property (nonatomic, strong) TEALDispatchBlock _Nullable assignedBlock;
+
+/**
+ *  Name of the dispatch service used to deliver track call.
+ *
+ */
+@property (nonatomic, strong) NSString * _Nullable dispatchServiceName;
+
+/**
+ *  The populated datasources available for mapping with the dispatch.
+ */
+@property (nonatomic, strong) NSDictionary * _Nonnull payload;
+
+/**
+ *  Time in Unix epoch of when the dispatch origin track call was called.
+ */
+@property (nonatomic) NSTimeInterval timestamp;
+
+/**
+ *  Status as human-readable text.
+ *
+ *  @param dispatchStatus TEALDispatchStatus of a dispatch.
+ *
+ *  @return NSString representation of the given dispatch status type.
+ */
++ (NSString * _Nonnull)stringFromDispatchStatus:(TEALDispatchStatus)dispatchStatus;
+
+/**
+ * Converts a TEALDispatch to a NSDictionary 
+ */
+
+- (NSDictionary * _Nonnull)asDictionary;
+
+/**
+ * Converts a NSDictionary to a TEALDispatch
+ */
+
++ (TEALDispatch * _Nonnull)fromDictionary:(NSDictionary* _Nullable)dictionary;
+
+/**
+ Adds dataSources to the payload.
+ 
+ @param dataSources NSDictionary to add to the dispatch.
+ */
+- (void)addDataSources:(NSDictionary *)dataSources;
+
+/**
+ Creates a TEALDispatch given a payload.
+ 
+ @param payload NSDictionary: payload
+ 
+ @returns TEALDispatch: the TEALDispatch.
+ */
++ (TEALDispatch * _Nonnull)dispatchForType:(TEALDispatchType)dispatchType withPayload:(NSDictionary * _Nonnull)payload;
+@end
---
+++ TealiumIOSLifecycle.podspec
@@ -0,0 +1,25 @@
+#
+# Be sure to run `pod lib lint TealiumIOSLifecycle.podspec' to ensure this is a
+# valid spec before submitting.
+#
+# Any lines starting with a # are optional, but their use is encouraged
+# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html
+#
+
+Pod::Spec.new do |s|
+    s.author           = { "Jonathan Wong" => "jonathan.wong@tealium.com" }
+    s.homepage         = "https://github.com/tealium/"
+    s.documentation_url = 'http://tealium.github.io/tealium-ios/'
+    s.frameworks = 'SystemConfiguration'
+    s.license      = { :type => "Commercial", :file => "LICENSE.txt" }
+    s.name             = "TealiumIOSLifecycle"
+    s.version          = "5.4.6"
+    s.requires_arc = true
+    s.social_media_url   = "https://twitter.com/tealium"
+    s.source           = { :git => "https://github.com/tealium/tealium-ios.git", :tag => s.version.to_s}
+    s.summary          = "A companion framework to TealiumIOS that adds Lifecycle tracking to an iOS application."
+    s.platform     = :ios, '8.0'
+    #s.public_header_files = 'TealiumIOS.framework/Headers/*.{h}'
+    s.vendored_frameworks = 'TealiumIOSLifecycle.framework'
+    s.xcconfig = {'HEADER_SEARCH_PATHS' => '$(PODS_ROOT)/Headers/Public/TealiumIOS_Lifecycle/'}
+end
