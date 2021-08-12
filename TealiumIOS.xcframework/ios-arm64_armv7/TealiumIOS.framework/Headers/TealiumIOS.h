8cb07aedc39ab42dd9e2dab66c5a2299fd9fe3ec
craigrouse <craig.rouse@gmail.com>
2021-08-12 17:53:24 +0100

    added xcframeworks

---
+++ TealiumIOS.xcframework/ios-arm64_armv7/TealiumIOS.framework/Headers/TealiumIOS.h
@@ -0,0 +1,42 @@
+//
+//  TealiumIOS.h
+//  TealiumIOS
+//
+//  Created by Jason Koo on 11/8/15.
+//  Copyright © 2015 Tealium Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TealiumIOS.
+FOUNDATION_EXPORT double TealiumIOSVersionNumber;
+
+//! Project version string for TealiumIOS.
+FOUNDATION_EXPORT const unsigned char TealiumIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TealiumIOS/PublicHeader.h>
+#import <TealiumIOS/Tealium.h>
+#import <TealiumIOS/Tealium+Collect.h>
+#import <TealiumIOS/Tealium+TagManagement.h>
+#import <TealiumIOS/Tealium+WatchKit.h>
+#import <TealiumIOS/TealiumDelegate.h>
+
+#import <TealiumIOS/TEALConfiguration+Collect.h>
+#import <TealiumIOS/TEALWebView.h>
+#import <TealiumIOS/TEALConfiguration+TagManagement.h>
+#import <TealiumIOS/TEALConfiguration.h>
+#import <TealiumIOS/TEALDataSourceConstants.h>
+#import <TealiumIOS/TEALWKConstants.h>
+#import <TealiumIOS/TEALConsentConstants.h>
+#import <TealiumIOS/TEALConsentManager.h>
+#import <TealiumIOS/TEALDispatch.h>
+#import <TealiumIOS/TEALLogLevels.h>
+#import <TealiumIOS/TEALRemoteCommandResponse.h>
+#import <TealiumIOS/TEALVisitorProfile.h>
+#import <TealiumIOS/TEALVisitorProfileCurrentVisit.h>
+#import <TealiumIOS/TEALVisitorProfileAudienceAttribute.h>
+#import <TealiumIOS/TEALVisitorProfileBadgeAttribute.h>
+#import <TealiumIOS/TEALVisitorProfileDateAttribute.h>
+#import <TealiumIOS/TEALVisitorProfileFlagAttribute.h>
+#import <TealiumIOS/TEALVisitorProfileMetricAttribute.h>
+#import <TealiumIOS/TEALVisitorProfilePropertyAttribute.h>
